5b230d8c6639b872e68d0924ac38eaf5c18f44e3
laura <mail@laura.fm>
2022-04-28 18:39:23 +0200

    Add: Configure db from $_SERVER for live on aws

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => $_SERVER['DATABASE_URL'] ?? env('DATABASE_URL'),
+            'host' => $_SERVER['DB_HOST'] ?? env('DB_HOST', '127.0.0.1'),
+            'port' => $_SERVER['DB_PORT'] ?? env('DB_PORT', '5432'),
+            'database' => $_SERVER['DB_DATABASE'] ?? env('DB_DATABASE', 'forge'),
+            'username' => $_SERVER['DB_USERNAME'] ?? env('DB_USERNAME', 'forge'),
+            'password' => $_SERVER['DB_PASSWORD'] ?? env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
