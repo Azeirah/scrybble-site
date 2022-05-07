434b2c8683c67f0c718294cd4ad706ea286eb34d
laura <mail@laura.fm>
2022-05-07 23:08:42 +0200

    Add: Padding to heading

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <h1 class="text-xl font-bold">{{$currentWorkingDirectory}}</h1>
+                        <h1 class="text-xl font-bold mb-1">{{$currentWorkingDirectory}}</h1>
                         <ul>
                             @foreach($ls as $item)
                                 <li class="flex items-center">
