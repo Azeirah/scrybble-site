ba2923ed75ac748cf8537452f5e3ca4630986ba8
laura <mail@laura.fm>
2022-05-22 19:48:16 +0200

    Add: Default to --modified-pdf for remarks extract

--- app/Services/RemarksService.php
+++ app/Services/RemarksService.php
@@ -9,11 +9,11 @@ use RuntimeException;
 class RemarksService {
 
     public function extractNotesAndHighlights(AbsolutePathInterface $sourceDirectory, AbsolutePathInterface
-    $targetDirectory, RemarksConfig $config) {
+    $targetDirectory, RemarksConfig $config): void {
         // docker run -v "$PWD/files":/store laauurraaa/remarks-bin /store /store --targets md
         $srcDir = $sourceDirectory->string();
         $targetDir = $targetDirectory->string();
-        $strCommand = "docker run -v \"$srcDir/\":/in -v \"$targetDir\":/out laauurraaa/remarks-bin /in /out --targets md";
+        $strCommand = "docker run -v \"$srcDir/\":/in -v \"$targetDir\":/out laauurraaa/remarks-bin /in /out --modified_pdf";
         exec($strCommand, $output, $result_code);
 
         if ($result_code !== 0) {
