77a13dc0c4531c2ce1dbedcec9c88ceea6879c6b
laura <mail@laura.fm>
2022-05-22 19:48:37 +0200

    Refactor: Restructure ProcessDownloadedZip job

--- app/Jobs/ProcessDownloadedZip.php
+++ app/Jobs/ProcessDownloadedZip.php
@@ -8,7 +8,9 @@ use App\Helpers\UserStorage;
 use App\Models\User;
 use App\Services\RemarksService;
 use Eloquent\Pathogen\AbsolutePath;
+use Eloquent\Pathogen\Exception\EmptyPathException;
 use Eloquent\Pathogen\Exception\InvalidPathStateException;
+use Eloquent\Pathogen\Exception\NonAbsolutePathException;
 use Eloquent\Pathogen\RelativePath;
 use Eloquent\Pathogen\RelativePathInterface;
 use Illuminate\Bus\Queueable;
@@ -16,6 +18,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use RuntimeException;
 
 class ProcessDownloadedZip implements ShouldQueue {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -38,34 +41,43 @@ class ProcessDownloadedZip implements ShouldQueue {
     /**
      * Execute the job.
      *
+     * @param RemarksService $remarksService
      * @return void
+     * @throws EmptyPathException
      * @throws InvalidPathStateException
-     * @throws \Eloquent\Pathogen\Exception\NonAbsolutePathException
+     * @throws NonAbsolutePathException
      */
     public function handle(RemarksService $remarksService): void {
-        /**
-         * Create a temporary folder and move the zip to this location. Might be useful to name this folder as the
-         * job's id
-         * This is to isolate the processing of each zip in one unique location. Mostly just for debugging purposes.
-         * Extract the zip
-         * Delete the zip
-         * Run remarks over the extracted files
-         * Insert a row in "sync" table
-         */
         $userStorage = UserStorage::get($this->user);
         $jobId = $this->job->getJobId();
+
         $jobdir = new RelativePath(['jobs', $jobId]);
         $to = $jobdir->joinAtoms('extractedFiles')->toRelative();
+
+        // 1. Create a temporary folder and move the zip to this location.
         FileManipulations::ensureDirectoryTreeExists($userStorage, $to);
-        if (!$userStorage->exists($jobId)) {
-            $userStorage->makeDirectory($jobId);
-        }
 
         // 2. Extract the zip
         FileManipulations::extractZip($userStorage, from: $this->zipLocation, to: $to);
 
+        // 3. Delete the zip
+        // TODO
+
+
+        // 4. Run remarks over the extracted files
         $absJobdir = AbsolutePath::fromString($userStorage->path($jobdir->string()));
         $absOutdir = AbsolutePath::fromString($userStorage->path($jobdir->joinAtoms('out')));
-        $remarksService->extractNotesAndHighlights($absJobdir, $absOutdir, $this->remarksConfig);
+        try {
+            $remarksService->extractNotesAndHighlights($absJobdir, $absOutdir, $this->remarksConfig);
+        } catch (RuntimeException $exception) {
+//            if ($this->user->config()->telemetryEnabled) {
+//                throw $exception;
+//            }
+        }
+
+        // 5. Zip the out dir
+        // 6. Upload zip to Z3
+        // 7. Unless user has telemetry=on && remarksService exception happened, delete temporary folder
+        // 8. Insert a row in "sync" table
     }
 }
