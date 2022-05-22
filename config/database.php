c083c41d34c15e61da9b6b85f709a4cb352c51f6
laura <mail@laura.fm>
2022-05-22 18:59:38 +0200

    Add: Read redis host from $_SERVER if present (for ebs prod)

--- config/database.php
+++ config/database.php
@@ -130,7 +130,7 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', $_SERVER['REDIS_HOST'] ?? '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
