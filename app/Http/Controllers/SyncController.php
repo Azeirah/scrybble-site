005aaa37d06e529e6693fb77456e8906fd828025
laura <mail@laura.fm>
2022-06-08 16:46:35 +0200

    Fix: Return jsonresponse instead of collection

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Sync;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\UnauthorizedException;
@@ -13,19 +14,20 @@ use Illuminate\Validation\UnauthorizedException;
  */
 class SyncController extends Controller {
     /**
-     * @return array
+     * @return JsonResponse
      */
-    public function index(): array {
+    public function index(): JsonResponse {
         $user = Auth::user();
 
         if ($user === null) {
             throw new UnauthorizedException('You need to be logged in in order to interact with the API');
         }
         $disk = Storage::disk('s3');
-        return Sync::forUser($user)
-                   ->get(['filename', 'S3_download_path'])
-                   ->map(fn($sync) => ['download_url' => $disk->temporaryUrl($sync->S3_download_path,
-                       now()->addMinutes(5)),
-                                       'filename' => $sync->filename]);
+        $results = Sync::forUser($user)
+                       ->get(['filename', 'S3_download_path'])
+                       ->map(fn($sync) => ['download_url' => $disk->temporaryUrl($sync->S3_download_path,
+                           now()->addMinutes(5)),
+                                           'filename' => $sync->filename]);
+        return response()->json($results);
     }
 }
