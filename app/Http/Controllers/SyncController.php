838929c2a881a2b48336fbf7abbeba88db0af60c
laura <mail@laura.fm>
2022-06-08 13:04:36 +0200

    Add: SyncController

---
+++ app/Http/Controllers/SyncController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Sync;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
+
+/**
+ *
+ */
+class SyncController extends Controller
+{
+    /**
+     * @return mixed
+     */
+    public function index(): mixed {
+        $user = Auth::user();
+
+        if ($user === null) {
+            throw new UnauthorizedException('You need to be logged in in order to interact with the API');
+        }
+        return Sync::forUser($user)->get();
+    }
+}
