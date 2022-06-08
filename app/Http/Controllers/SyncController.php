c82b2dccd734763c115cd26e18b89858b5f4cd0f
laura <mail@laura.fm>
2022-06-08 16:40:26 +0200

    Add: Provide download urls for S3 objects

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Sync;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\UnauthorizedException;
 
 /**
@@ -12,14 +13,19 @@ use Illuminate\Validation\UnauthorizedException;
  */
 class SyncController extends Controller {
     /**
-     * @return mixed
+     * @return array
      */
-    public function index(): mixed {
+    public function index(): array {
         $user = Auth::user();
 
         if ($user === null) {
             throw new UnauthorizedException('You need to be logged in in order to interact with the API');
         }
-        return Sync::forUser($user)->get(['filename', 'S3_download_path', 'updated_at']);
+        $disk = Storage::disk('s3');
+        return Sync::forUser($user)
+                   ->get(['filename', 'S3_download_path'])
+                   ->map(fn($sync) => ['download_url' => $disk->temporaryUrl($sync->S3_download_path,
+                       now()->addMinutes(5)),
+                                       'filename' => $sync->filename]);
     }
 }
