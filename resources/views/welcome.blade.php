9f2a638251fbce6ba39ef8bc89b980881cc8a363
laura <mail@laura.fm>
2022-08-10 18:43:17 +0200

    Add: Update homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Scrybble</title>
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
@@ -61,7 +61,7 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
     <header class="mb-auto">
         <div>
-            <h3 class="float-md-start mb-0">Cover</h3>
+            <h3 class="float-md-start mb-0">Scrybble</h3>
             <nav class="nav nav-masthead justify-content-center float-md-end">
                 <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
                 <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
