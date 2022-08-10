b57355ff524ae105c6517f8208a29780c4362ce1
laura <mail@laura.fm>
2022-08-10 16:36:59 +0200

    Add: Homepage

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer {{ $attributes->class(['mt-auto text-white-50']) }}>
+    <p>© {{date_format(now(), 'Y')}} Streamsoft. Streamsoft is a sole-proprietorship registered in the Netherlands.</p>
+</footer>
