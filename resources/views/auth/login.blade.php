7271a18d56a98c85e56ec7d7ea7aa727300c773a
Laura <mail@laura.fm>
2022-12-04 20:51:49 +0100

    Refactor: Abstract some css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="h-100 container d-flex align-items-center justify-content-center">
+    <div class="page-centering-container">
         <div class="col-md-8">
-            <div class="card text-white bg-dark">
+            <div class="card-dark">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
