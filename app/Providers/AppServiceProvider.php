28f0dcf6b0d866c03467921fda1202a03e72fa0c
Laura <mail@laura.fm>
2022-12-04 20:37:23 +0100

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,16 +4,15 @@ namespace App\Providers;
 
 use App\Services\RMapi;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         $this->app->singleton(RMapi::class, function () {
             return new RMapi();
         });
@@ -24,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot() {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
