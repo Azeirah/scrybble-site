ab6ae983437c40c2a4e212b8511b1a4d85ab5030
Laura <mail@laura.fm>
2022-12-19 00:41:39 +0100

    Add: Consider syncs older than X minutes "errored"

--- app/Http/Controllers/InspectSyncController.php
+++ app/Http/Controllers/InspectSyncController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sync;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class InspectSyncController extends Controller {
     public function index() {
+        $grace_period_in_minutes = 5;
         $collection = Sync::select(['filename', 'created_at', 'completed'])
                           ->forUser(Auth::user())
                           ->limit(10)
@@ -15,7 +17,12 @@ class InspectSyncController extends Controller {
                               'filename' => $syncItem->filename,
                               'created_at' => $syncItem->created_at->diffForHumans(),
                               'completed' => $syncItem->completed,
-                              'error' => $syncItem->logs()->where('severity', 'error')->count() > 0
+                              'error' => !$syncItem->completed
+                                  && (Carbon
+                                          ::now()
+                                          ->addMinutes($grace_period_in_minutes)
+                                          ->greaterThan($syncItem->created_at) ||
+                                      $syncItem->logs()->where('severity', 'error')->count() > 0)
                           ]);
         return response()->json(
             $collection
