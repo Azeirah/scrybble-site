d135efbd042bd9bfb4aded2e13fad4123adcf42d
Laura <mail@laura.fm>
2022-12-18 20:28:50 +0100

    Add: Change default output variant to non-modified

--- app/DataClasses/RemarksConfig.php
+++ app/DataClasses/RemarksConfig.php
@@ -2,13 +2,30 @@
 
 namespace App\DataClasses;
 
+use JsonException;
 use JsonSerializable;
 
+/**
+ * @property $modified_only Skips pages without scribbles or annotations
+ */
 class RemarksConfig implements JsonSerializable {
 
-    public function __construct() {}
+    public function __construct(public bool $modified_only = false) {}
 
+    /**
+     * @throws JsonException
+     */
     public function jsonSerialize() {
-        // TODO: Implement jsonSerialize() method.
+        return json_encode([
+            'modified_only' => $this->modified_only
+        ], JSON_THROW_ON_ERROR);
+    }
+
+    public function toRemarksParams() {
+        $params = collect();
+        if ($this->modified_only) {
+            $params->add("--modified_pdf");
+        }
+        return $params->implode(' ');
     }
 }
--- app/Services/RemarksService.php
+++ app/Services/RemarksService.php
@@ -18,12 +18,12 @@ class RemarksService {
      * @param RemarksConfig $config
      * @return void
      */
-    public function extractNotesAndHighlights(AbsolutePathInterface $sourceDirectory, AbsolutePathInterface
-                                                                    $targetDirectory, RemarksConfig $config): void {
+    public function extractNotesAndHighlights(AbsolutePathInterface $sourceDirectory, AbsolutePathInterface $targetDirectory, RemarksConfig $config): void {
         // docker run -v "$PWD/files":/store laauurraaa/remarks-bin /store /store --targets md
         $source_dir = $sourceDirectory->string();
         $target_dir = $targetDirectory->string();
-        $command = "docker run -v \"$source_dir/\":/in -v \"$target_dir\":/out laauurraaa/remarks-bin /in /out --modified_pdf";
+        $params = $config->toRemarksParams();
+        $command = "docker run -v \"$source_dir/\":/in -v \"$target_dir\":/out laauurraaa/remarks-bin /in /out $params";
         exec($command, $output, $result_code);
 
         if ($result_code !== 0) {
