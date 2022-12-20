3205c35af593716b6b423952f6622f7f83dfbea7
Laura <mail@laura.fm>
2022-12-20 21:44:07 +0100

    Add: Update roadmap

--- resources/ts/pages/Roadmap.tsx
+++ resources/ts/pages/Roadmap.tsx
@@ -22,20 +22,15 @@ export function Roadmap() {
                 <td>Support PDFs</td>
                 <td>Allow synchronization of PDF files from ReMarkable to Obsidian</td>
             </tr>
-            <tr className="table-warning">
-                <td>No</td>
+            <tr className="table-success">
+                <td>Yes (beta)</td>
                 <td>Support .epub format</td>
-                <td>Idem, see above</td>
+                <td>Allow synchronization of .epub files from ReMarkable to Obsidian</td>
             </tr>
             <tr className="table-warning">
-                <td>No</td>
-                <td>Support .mobi format</td>
-                <td>Idem, see above</td>
-            </tr>
-            <tr className="table-danger">
-                <td>No</td>
-                <td>Quick sheets and notebooks</td>
-                <td>Unlikely to be added soon.</td>
+                <td>Maybe</td>
+                <td>Quick sheets</td>
+                <td>Quick sheets are currently not supported, but support is likely to be added soon.</td>
             </tr>
             <tr className="table-group-divider">
                 <td colSpan={3} className="table-dark text-center">
@@ -46,7 +41,7 @@ export function Roadmap() {
                 <td>No</td>
                 <td>Tags</td>
                 <td>ReMarkable recently released a tags feature, it would be great if the synchronized pages contain
-                    these references somehow.
+                    these references.
                 </td>
             </tr>
             <tr className="table-group-divider">
@@ -54,17 +49,12 @@ export function Roadmap() {
                     <h4 style={{marginBottom: 0}}>Website user interface</h4>
                 </td>
             </tr>
-            <tr className="table-warning">
-                <td>No</td>
+            <tr className="table-success">
+                <td>Yes (beta)</td>
                 <td>Show synchronization status</td>
-                <td>There are multiple steps to synchronize a file from RM to Obsidian, there is currently no
-                    feedback on how this process is proceeding .
+                <td>There are multiple steps to synchronize a file from RM to Obsidian, since mid-December, we have a
+                    "Sync status" page to inspect what does and doesn't sync as expected.
                 </td>
-            </tr>
-            <tr className="table-warning">
-                <td>No</td>
-                <td>Show synchronization status</td>
-                <td>Idem, see above</td>
             </tr>
             <tr className="table-group-divider">
                 <td colSpan={3} className="table-dark text-center">
@@ -81,17 +71,27 @@ export function Roadmap() {
                     <h4 style={{marginBottom: 0}}>Synchronization options</h4>
                 </td>
             </tr>
-            <tr className="table-success">
-                <td>yes</td>
+            <tr className="table-warning">
+                <td>No</td>
+                <td>Automatically sync on file change</td>
+                <td>At the moment, you'll have to log in to the web interface and click a file to start the
+                    synchronisation process. It would be preferable if this happened automatically, this will be added
+                    at a later time.
+                </td>
+            </tr>
+            <tr className="table-warning">
+                <td>No</td>
                 <td>Only sync pages with notes</td>
                 <td>An e-book can have 400 pages, where only 17 of them contain annotations. With this option, only
                     those 17 pages will show up in your vault.
                 </td>
             </tr>
             <tr className="table-warning">
-                <td>no</td>
-                <td>Sync all pages</td>
-                <td>The opposite of the previous option, include all pages every time.</td>
+                <td>No</td>
+                <td>Export highlights to markdown</td>
+                <td>Obsidian is best for working with text, with this option you will be able to choose if you want to
+                    export your highlights to a plain-text markdown file alongside the pdf.
+                </td>
             </tr>
             </tbody>
         </table>
