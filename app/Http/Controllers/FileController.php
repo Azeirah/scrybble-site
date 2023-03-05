6c9a9658217ebf84b3b7803da24dd50a0c84f7fb
Laura <mail@laura.fm>
2023-03-05 16:14:22 +0100

    Log input filename when starting sync

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -26,7 +26,7 @@ class FileController extends Controller {
         $user = Auth::user();
         $input_filename = $request->get('file');
         $sync_context = new SyncContext($input_filename, $user, new RemarksConfig());
-        Log::info("user=`$user` requested file file=`input_filename`; Dispatching `DownloadRemarkableFileJob`");
+        Log::info("user=`$user` requested file file=`$input_filename`; Dispatching `DownloadRemarkableFileJob`");
         DownloadRemarkableFileJob::dispatch($sync_context);
     }
 }
