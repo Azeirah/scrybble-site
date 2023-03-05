5529c59a8b1fe07cdd2abe4209e0b9fd4bc9ec80
Laura <mail@laura.fm>
2023-03-05 16:14:04 +0100

    Schedule horizon metrics

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,17 +5,16 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
+    protected function schedule(Schedule $schedule) {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
@@ -23,9 +22,8 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    protected function commands() {
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
