1f72425ea1cef390691509f89565b9d0f8e3c2af
Laura <mail@laura.fm>
2023-03-11 20:38:54 +0100

    Restyle dashboard table, add sync action and sync settings

--- resources/ts/components/feature/RMFileTree/RMFileTree.tsx
+++ resources/ts/components/feature/RMFileTree/RMFileTree.tsx
@@ -1,40 +1,30 @@
 import React, {useCallback} from "react"
 import "./RMFileTree.scss"
-import {Link, useLocation} from "react-router-dom"
+import {Link, useLocation, useNavigate} from "react-router-dom"
 import {Directory, File, useRMFileTreeQuery, useSelectFileForSyncMutation} from "../../../store/api/apiRoot"
 import toast from "react-hot-toast"
 import _ from "lodash"
+import {faFile, faFolder, faHighlighter} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
-const iconStyle = {width: "24px", height: "24px", display: "inline-block"}
+const DirectoryIcon = () => <FontAwesomeIcon icon={faFolder}/>
 
-const DirectoryIcon = () => <svg className="bi" fill="none"
-                                 style={iconStyle}
-                                 viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-    <path strokeLinecap="round" strokeLinejoin="round"
-          d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
-</svg>
-
-const FileIcon = () => <svg style={iconStyle} fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-    <path strokeLinecap="round" strokeLinejoin="round"
-          d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"/>
-</svg>
+const FileIcon = () => <FontAwesomeIcon icon={faFile}/>
 
 function DirectoryItem({item}: { item: Directory }) {
-    return <>
-        <DirectoryIcon/> <Link to={`/dashboard?path=${item.path}`}>{item.name}</Link>
-    </>
+    return <span>
+        <Link to={`/dashboard?path=${item.path}`}>{item.name}</Link>
+    </span>
 }
 
 function FileItem({item}: { item: File }) {
-    const [selectForSync, {}] = useSelectFileForSyncMutation()
+    return <span>{item.name}</span>
+}
 
-    const syncFile = useCallback(_.debounce(() => {
-        toast.success(`File ${item.name} will be synced!`)
-        selectForSync(item.path)
-    }, 1000), [item, selectForSync])
-
-    return <><FileIcon/><span onClick={syncFile}>{item.name}</span></>
+function SyncSettings() {
+    return <div role="group" aria-label="Synchronization settings" className="btn-group btn-group-sm">
+        <button className="btn btn-primary"><FontAwesomeIcon icon={faHighlighter}/></button>
+    </div>
 }
 
 export default function RMFileTree() {
@@ -42,18 +32,54 @@ export default function RMFileTree() {
     const params = new URLSearchParams(search)
     const path = params.get("path")
     const {data: filetree, isLoading} = useRMFileTreeQuery(path ?? "/")
+    const navigate = useNavigate()
+
+    const [selectForSync, {}] = useSelectFileForSyncMutation()
+
+    const syncFile = useCallback(_.debounce((item: File) => {
+        toast.success(`File ${item.name} will be synced!`)
+        selectForSync(item.path)
+    }, 1000), [selectForSync])
 
     return <div className="container">
         {isLoading ? "loading" : <div id="filetree">
-            <h2>{filetree.cwd}</h2>
-            <ul>
+            <table className="table table-dark table-sm table-hover table-striped align-middle caption-top">
+                <caption>{filetree.cwd}</caption>
+                <colgroup>
+                    <col width="0*"/>
+                    <col/>
+                    <col width="0*"/>
+                </colgroup>
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>File or directory</th>
+                    <th>Sync settings</th>
+                </tr>
+                </thead>
+                <tbody>
                 {filetree.items.map((item) => {
                     if (item.type === "d") {
-                        return <li><DirectoryItem item={item as Directory}/></li>
+                        return <tr className="directory" role="link"
+                                   onClick={() => navigate(`/dashboard?path=${item.path}`)}>
+                            <td></td>
+                            <td><DirectoryItem item={item as Directory}/></td>
+                            <td></td>
+                        </tr>
                     }
-                    return <li><FileItem item={item as File}/></li>
+                    return <tr>
+                        <td style={{width: "fit-content", padding: "8px"}}>
+                            <button className="btn btn-success btn-sm" onClick={() => {
+                                syncFile(item as File)
+                            }}>Sync now
+                            </button>
+                        </td>
+                        <td className="file"><FileItem item={item as File}/></td>
+                        <td style={{textAlign: "right", padding: "0 8px"}}><SyncSettings/></td>
+                    </tr>
                 })}
-            </ul>
+                </tbody>
+            </table>
         </div>}
     </div>
 }
