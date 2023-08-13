78cb6433887f45dbe02238f269c95f1153025849
Laura <mail@laura.fm>
2023-08-13 18:42:54 +0200

    Update remarks with obsidian md output

--- app/Services/RemarksService.php
+++ app/Services/RemarksService.php
@@ -12,7 +12,7 @@ use RuntimeException;
  */
 class RemarksService
 {
-    private string $remarks_version = "0.3.8";
+    private string $remarks_version = "0.3.9";
 
     /**
      * @param AbsolutePathInterface $sourceDirectory
