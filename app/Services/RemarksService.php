ae101f98b8ccb90d4666fc33aa414ccd43b958fc
Laura <mail@laura.fm>
2023-08-23 23:15:44 +0200

    use more recent version of remarks

--- app/Services/RemarksService.php
+++ app/Services/RemarksService.php
@@ -12,7 +12,7 @@ use RuntimeException;
  */
 class RemarksService
 {
-    private string $remarks_version = "0.3.14";
+    private string $remarks_version = "0.3.15";
 
     /**
      * @param AbsolutePathInterface $sourceDirectory
