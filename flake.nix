5592701e125542bd314d91405f47577482d0cfa9
Laura Brekelmans <mail@laura.fm>
2024-12-21 16:51:57 +0100

    Move to Nix 24.05, add bun

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "rmapi - ReMarkable Cloud API CLI client";
+  description = "Scrybble development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,20 +10,46 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.buildGoModule {
+
+        rmapiBuild = pkgs.buildGoModule {
           pname = "rmapi";
           version = "0.0.25";
+
+          CGO_ENABLED = 0;
+          # strips debug information
+          ldflags = [ "-s" "-w" ];
 
           src = pkgs.fetchFromGitHub {
             owner = "ddvk";
             repo = "rmapi";
-            rev = "master";
+            rev = "master"; # TODO: Should be configurable with a flake command input.
             sha256 = "sha256-GhyZRwsywnFQ4GABbbSOtjVgUuIn5k4iaqPfiyVOAIs=";
           };
 
           vendorHash = "sha256-5m3/XFyBEWM8UB3WylkBj+QWI5XsnlVD4K3BhKVVCB4=";
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_22
+            pkgs.bun
+          ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "rmapi-bundle";
+          version = "0.0.25";
+
+          buildInputs = [ rmapiBuild ];
+
+          dontUnpack = true;
+
+          installPhase = ''
+            mkdir -p $out/binaries
+            cp ${rmapiBuild}/bin/rmapi $out/binaries/rmapi
+            chmod 755 $out/binaries/rmapi
+          '';
         };
       }
     );
